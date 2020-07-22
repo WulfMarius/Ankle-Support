7ed7c6eaa46c06fd6e15b56f6f849e9e95779854
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2020-07-23 01:26:33 +0200

    MelonLoader already catches and logs exceptions in Harmony patches

--- src/Patches.cs
+++ src/Patches.cs
@@ -1,61 +1,36 @@
 ﻿using Harmony;
-using UnityEngine;
 
 namespace AnkleSupport
 {
     [HarmonyPatch(typeof(GameManager), "Awake")]
-    internal class GameManager_Awake
+    internal static class GameManager_Awake
     {
         private static void Postfix()
         {
-            try
-            {
-                Implementation.Initialize();
-            }
-            catch (System.Exception e)
-            {
-                HUDMessage.AddMessage("[" + Implementation.NAME + "] Could not initialize.");
-                Debug.Log(e.ToString());
-            }
+            Implementation.Initialize();
         }
     }
 
     [HarmonyPatch(typeof(PlayerManager), "PutOnClothingItem")]
-    internal class PlayerManager_PutOnClothingItem
+    internal static class PlayerManager_PutOnClothingItem
     {
         private static void Postfix(GearItem gi)
         {
             if (gi.m_ClothingItem != null && gi.m_ClothingItem.m_Region == ClothingRegion.Feet)
             {
-                try
-                {
-                    Implementation.UpdateAnkleSupport();
-                }
-                catch (System.Exception e)
-                {
-                    HUDMessage.AddMessage("[" + Implementation.NAME + "] Could not update ankle protection.");
-                    Debug.Log(e.ToString());
-                }
+                Implementation.UpdateAnkleSupport();
             }
         }
     }
 
     [HarmonyPatch(typeof(PlayerManager), "TakeOffClothingItem")]
-    internal class PlayerManager_TakeOffClothingItem
+    internal static class PlayerManager_TakeOffClothingItem
     {
         private static void Postfix(GearItem gi)
         {
             if (gi.m_ClothingItem != null && gi.m_ClothingItem.m_Region == ClothingRegion.Feet)
             {
-                try
-                {
-                    Implementation.UpdateAnkleSupport();
-                }
-                catch (System.Exception e)
-                {
-                    HUDMessage.AddMessage("[" + Implementation.NAME + "] Could not update ankle protection.");
-                    Debug.Log(e.ToString());
-                }
+                Implementation.UpdateAnkleSupport();
             }
         }
     }
